b548bd6c320b3066a5551b74d4b891945104d1cc
Hasan Yousef <hasan.ajsf@gmail.com>
2019-09-05 19:47:47 +0300

    start

---
+++ Rust/target/debug/deps/tutorials-cdf3d59031874163.d
@@ -0,0 +1,5 @@
+/Users/hasan/Documents/tutorials/Rust/target/debug/deps/tutorials-cdf3d59031874163: src/main.rs
+
+/Users/hasan/Documents/tutorials/Rust/target/debug/deps/tutorials-cdf3d59031874163.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/target/debug/tutorials.d
@@ -0,0 +1,1 @@
+/Users/hasan/Documents/tutorials/Rust/target/debug/tutorials: /Users/hasan/Documents/tutorials/Rust/src/main.rs
